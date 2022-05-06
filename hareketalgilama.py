051232c3daf744419bb9275baffc49339b49f49d
Melih Korkmaz <melihkorkmaz46@gmail.com>
2022-05-06 18:13:56 +0300

    motion cap baslandi

--- hareketalgilama.py
+++ hareketalgilama.py
@@ -43,6 +43,20 @@ while(True):
     cv2.imshow('window',cv2.cvtColor(diff, cv2.COLOR_BGR2RGB))
     cv2.resizeWindow("window", 600, 500) 
     cv2.moveWindow("window", 1024, 0)
+    
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+    dilated = cv2.dilate(thresh, None, iterations=3)
+    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        (x, y, w, h) = cv2.boundingRect(contour)
+
+        if cv2.contourArea(contour) < 900:
+            continue
+        cv2.rectangle(screen_grab, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        #cv2.putText(screen_grab, "Status: {}".format('Movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+    
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
